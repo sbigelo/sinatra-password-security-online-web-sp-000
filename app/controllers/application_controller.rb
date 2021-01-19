8bfc1ec498f86800b3292af3f32fb21ed39ede2c
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-19 18:40:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,10 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
+		if user
+			redirect "/success"
+		else
+			redirect "/failure"
 	end
 
 	get "/success" do
